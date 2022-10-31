aca344e9430038153dd8f746ed92525d5bacb7f4
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-31 10:21:54 +0300

    completed assignment

--- author.rb
+++ author.rb
@@ -1,39 +1,39 @@
-class Author
-    attr_reader :name
-   def initialize(name)
-    @name=name
-   end
-end
+# class Author
+#     attr_reader :name
+#    def initialize(name)
+#     @name=name
+#    end
+# end
 
-class Magazine < Author 
-    attr_accessor :name ,:category
-    @@all =[]
-    def initialize(name, category)
-        @name=name
-        @category=category
-        @@all << self
+# class Magazine < Author 
+#     attr_accessor :name ,:category
+#     @@all =[]
+#     def initialize(name, category)
+#         @name=name
+#         @category=category
+#         @@all << self
 
-    end
-    def self.all
-        @@all
-      end
-      def self.readmagazine
-        self.all.each do |magazine|
-          puts magazine.name
-        end
+#     end
+#     def self.all
+#         @@all
+#       end
+#       def self.readmagazine
+#         self.all.each do |magazine|
+#           puts magazine.name
+#         end
 
-end
+# end
 
-class Article < Author
-     attr_reader :title
-     @@all =[]
-    def initialize(author,magazine,title)
-     @author = author 
-     @magazine=magazine
-     @title=title
-     @@all << self
-    end
-    def self.all
-        @@all
-      end
-    end
+# class Article < Author
+#      attr_reader :title
+#      @@all =[]
+#     def initialize(author,magazine,title)
+#      @author = author 
+#      @magazine=magazine
+#      @title=title
+#      @@all << self
+#     end
+#     def self.all
+#         @@all
+#       end
+#     end
--- solution.rb
+++ solution.rb
@@ -7,11 +7,20 @@ class Author
 
   #  `Author#articles`
      def articles
-     Article.all.select{|article|article.author.self}
+     Article.all.select{|article|article.author.self.name}
      end
      def magazines
-      articles.map(magazine).uniq
+      articles.map(|article|article.magazine).uniq
      end
+    #  `Author#add_article(magazine, title)`
+     def add_article(magazine,title)
+      Article.create(self,magazine,tittle)
+     end
+    #`Author#topic_areas`
+     def topic_areas
+      magazines.collect{|magazine|magazine.category}.uniq
+     end
+
 end
 
 class Magazine  
@@ -26,15 +35,36 @@ class Magazine
     def self.all
         @@all
       end
-      def self.readmagazine
-        self.all.each do |magazine|
-          puts magazine.name
+      def magazine_articles
+        Article.all.select{|article|article.magazine.name==self.name}
+        end
+
+        # `Magazine#contributors`
+        def contributors
+          magazine_articles.collect{|articles|articles.author}.uniq
+        end
+        # `Magazine#article_titles`
+        def article_titles
+          magazine_articles.collect{|article|article.title}
+        end
+        # `Magazine.find_by_name(name)`
+        def self.find_by_name(name)
+        magazine={|magazine|magazine.name==name}
+        magazine.name
+        end
+        # `Magazine#contributing_authors`
+        def contributing_authors
+          author=magazine_articles.map {|article|article.author}.uniq
+            if author.artilce.length>2
+              article.author
+            end
+        
         end
 
 end
 
 class Article 
-     attr_reader :title
+     attr_reader :magazine, :title
      @@all =[]
     def initialize(author,magazine,title)
      @author = author 
@@ -44,5 +74,8 @@ class Article
     end
     def self.all
         @@all
+      end
+      def author
+        @author.name
       end
     end
