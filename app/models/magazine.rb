19f7a92185118f2ad4e9ac6de93354b2345fbf8c
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-02 22:00:57 +0300

    redone the challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,35 @@
 class Magazine
   attr_accessor :name, :category
-
-
-  def initialize(name, category)
+  @@all = []
+  @@count = 0
+  def initialize(name:, category:)
     @name = name
     @category = category
-
+    @@count =+1
+    @@all << self
   end
-
-
+  def self.count
+    @@count
+  end
+  def self.all
+    @@all
+  end
+  def articles
+    Article.all.filter {|article|article.magazine == self}
+  end
+  def articles_titles
+    self.articles.map{|articles|article.title}
+  end
+  
+  def contributors
+    self.articles.map{|article|article.author}.uniq
+  end
+  def contributing_authors
+    self.contibutors.filter{|author|author.articles.count >2}
+  end
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+    end
+  end
 end
--- solution.rb
+++ solution.rb
@@ -1,81 +1,75 @@
 # Please copy/paste all three classes into this file to submit your solution!
+require 'pry'
+class Article
+  attr_accessor :author,:magazine, :title
+  @@all = []
+  def initialize(author,magazine,title)
+    @author = author
+    @magazine = magazine
+    @title = title
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
 class Author
-    attr_reader :name
-   def initialize(name)
-    @name=name
-   end
-
-  #  `Author#articles`
-     def articles
-     Article.all.select{|article|article.author.self.name}
-     end
-     def magazines
-      articles.map(|article|article.magazine).uniq
-     end
-    #  `Author#add_article(magazine, title)`
-     def add_article(magazine,title)
-      Article.create(self,magazine,tittle)
-     end
-    #`Author#topic_areas`
-     def topic_areas
-      magazines.collect{|magazine|magazine.category}.uniq
-     end
-
+  attr_accessor :name
+   @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def articles
+    Article.all.filter{|article|article.author == self}
+  end
+  def magazines
+    self.articles.map do |article|
+    article.magazine
+    end.uniq
+  end
+  def add_article(title,magazine_name,magazine_category)
+    Article.new(self,title,magazine_name,magazine_category)
+  end
+  def topic_areas
+    self.magazine.map{|magazine|magazine.category}.uniq
+  end
 end
-
-class Magazine  
-    attr_accessor :name ,:category
-    @@all =[]
-    def initialize(name, category)
-        @name=name
-        @category=category
-        @@all << self
-
+class Magazine
+  attr_accessor :name, :category
+  @@all = []
+  @@count = 0
+  def initialize(name:, category:)
+    @name = name
+    @category = category
+    @@count =+1
+    @@all << self
+  end
+  def self.count
+    @@count
+  end
+  def self.all
+    @@all
+  end
+  def articles
+    Article.all.filter {|article|article.magazine == self}
+  end
+  def articles_titles
+    self.articles.map{|articles|article.title}
+  end
+  def contributors
+    self.articles.map{|article|article.author}.uniq
+  end
+  def contributing_authors
+    self.contibutors.filter{|author|author.articles.count >2}
+  end
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
     end
-    def self.all
-        @@all
-      end
-      def magazine_articles
-        Article.all.select{|article|article.magazine.name==self.name}
-        end
-
-        # `Magazine#contributors`
-        def contributors
-          magazine_articles.collect{|articles|articles.author}.uniq
-        end
-        # `Magazine#article_titles`
-        def article_titles
-          magazine_articles.collect{|article|article.title}
-        end
-        # `Magazine.find_by_name(name)`
-        def self.find_by_name(name)
-        magazine={|magazine|magazine.name==name}
-        magazine.name
-        end
-        # `Magazine#contributing_authors`
-        def contributing_authors
-          author=magazine_articles.map {|article|article.author}.uniq
-            if author.artilce.length>2
-              article.author
-            end
-        
-        end
-
+  end
 end
-
-class Article 
-     attr_reader :magazine, :title
-     @@all =[]
-    def initialize(author,magazine,title)
-     @author = author 
-     @magazine=magazine
-     @title=title
-     @@all << self
-    end
-    def self.all
-        @@all
-      end
-      def author
-        @author.name
-      end
-    end
+binding.pry
